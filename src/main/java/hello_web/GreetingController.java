fdff99648cbe09d513bfb560216ba9e4e97e016c
Internet_shop <trostnick97@mail.ru>
2018-09-14 10:10:33 +0300

    hello_web

---
+++ src/main/java/hello_web/GreetingController.java
@@ -0,0 +1,19 @@
+package hello_web;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private static long counter = 0;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(++counter,
+                String.format(template, name));
+    }
+}
