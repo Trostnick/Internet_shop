db2b8cf965378be9b26f92ebbefd34cfed5e3b4a
Internet_shop <trostnick97@mail.ru>
2018-09-20 11:57:06 +0300

    hello_web + RestTemplate

--- src/main/java/hello_web/GreetingController.java
+++ src/main/java/hello_web/GreetingController.java
@@ -4,16 +4,19 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private static long counter = 0;
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(++counter,
-                String.format(template, name));
+    public Quote greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject(
+                "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        return quote;
     }
 }
